cafd288a1ed72019dbee282eacd4a34c6317bc74
o-az <omaraziz.dev@proton.me>
2025-09-15 13:55:10 -0700

    chore: fmt

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,12 @@ import { proxy } from 'hono/proxy'
 
 import { app } from '#setup.ts'
 
-app.get('/', context => context.json({
-  routes: [
-    '/',
-    '/cat'
-  ],
-  version: context.env.APP_VERSION
-}))
+app.get('/', context =>
+  context.json({
+    routes: ['/', '/cat'],
+    version: context.env.APP_VERSION
+  })
+)
 
 app.get('/cat', async () =>
   proxy('https://api.ai-cats.net/v1/cat', {
