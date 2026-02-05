b12d043cd444d7fe0e54ca7555cf8648bbe8ca38
o-az <omaraziz.dev@proton.me>
2026-02-04 23:38:23 -0800

    fix: add cat

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { csrf } from 'hono/csrf'
+import { proxy } from 'hono/proxy'
 import { timeout } from 'hono/timeout'
 import { prettyJSON } from 'hono/pretty-json'
 
@@ -9,11 +10,17 @@ app.use(csrf())
 app.use(prettyJSON())
 app.use('*', timeout(5_000))
 
-app.get('/', context =>
-  context.json({
-    routes: ['/', '/cat'],
-    version: context.env.APP_VERSION,
-  }),
-)
+app
+  .get('/', context =>
+    context.json({
+      routes: ['/', '/cat'],
+      version: context.env.APP_VERSION,
+    }),
+  )
+  .get('/cat', async () =>
+    proxy('https://api.ai-cats.net/v1/cat', {
+      headers: { 'Content-Type': 'image/jpg' },
+    }),
+  )
 
 export default app satisfies ExportedHandler<Cloudflare.Env>
